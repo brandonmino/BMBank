ab98a64858dde77f6203980d59762cc66ca2ec91
Brandon Mino <43628774+brandonmino@users.noreply.github.com>
2021-09-08 15:16:45 -0400

    fix: update typos and swagger

--- src/main/java/com/bm/bank/exceptions/ExcessiveWithdrawalException.java
+++ src/main/java/com/bm/bank/exceptions/ExcessiveWithdrawalException.java
@@ -3,7 +3,7 @@ package com.bm.bank.exceptions;
 //Exception for trying to withdraw more funds than you have
 public class ExcessiveWithdrawalException extends BMBankWithdrawalException {
     public ExcessiveWithdrawalException() {
-        super(3001, "Withdraw was blocked as you attempted to withdraw more than your current balance");
+        super(3001, "Withdrawal was blocked as you attempted to withdraw more than your current balance");
     }
 
     public ExcessiveWithdrawalException(int code, String message) {
--- src/main/java/com/bm/bank/exceptions/NegativeWithdrawalException.java
+++ src/main/java/com/bm/bank/exceptions/NegativeWithdrawalException.java
@@ -3,7 +3,7 @@ package com.bm.bank.exceptions;
 //Exception for trying to withdraw more funds than you have
 public class NegativeWithdrawalException extends BMBankDepositException {
     public NegativeWithdrawalException() {
-        super(3000, "Withdraw was blocked as you attempted to withdraw a negative amount");
+        super(3000, "Withdrawal was blocked as you attempted to withdraw a negative amount");
     }
 
     public NegativeWithdrawalException(int code, String message) {
